38692600eafc3c5f9b69a7dc6cd9efafa2add7c0
Fernando Díaz <feeldiac@gmail.com>
2022-08-27 22:17:09 -0500

    Added tailwind config

---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.jsx"], //Purge config: https://tailwindcss.com/docs/upgrade-guide#configure-content-sources
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
