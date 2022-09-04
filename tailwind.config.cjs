0f98295e5a980a0926048e37d52e8c10e535d3d4
Fernando Díaz <feeldiac@gmail.com>
2022-09-03 23:45:15 -0500

    Update tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.jsx"], //Purge config: https://tailwindcss.com/docs/upgrade-guide#configure-content-sources
+  content: ["index.html", "./src/**/*.jsx"], //Purge config: https://tailwindcss.com/docs/upgrade-guide#configure-content-sources
   theme: {
     extend: {},
   },
