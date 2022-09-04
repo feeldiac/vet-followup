c8a86e0c65909db26bfa5f8c2497784c49a031c3
Fernando Díaz <feeldiac@gmail.com>
2022-09-03 20:14:13 -0500

    Add edit and delete buttons

--- src/components/Patient.jsx
+++ src/components/Patient.jsx
@@ -23,6 +23,19 @@ const Patient = ({ patient }) => {
             <p className='font-bold mb-3 text-gray-700 '>Symptoms: {''}
                 <span className='font-normal normal-case'>{symptoms}</span>
             </p>
+
+            <div className="flex justify-between mt-10">
+                <button
+                    type="button"
+                    className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold rounded-lg">
+                    Edit
+                </button>
+                <button
+                    type="button"
+                    className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold rounded-lg">
+                    Delete
+                </button>
+            </div>
         </div>
     )
 }
